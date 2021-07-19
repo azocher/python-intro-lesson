c8f71905376ca0ed6ea96bd1a9307585ca08d297
Anna Zocher <anna.zocher@gmail.com>
2021-07-19 11:38:45 -0700

    main done

---
+++ main.py
@@ -0,0 +1,51 @@
+# hello world print example
+#print("👋 Hello, world!")
+
+# variable examples
+name = "Anna"
+address_num = 1435
+has_dog = True
+
+# print(name)
+# print(address_num)
+# print(has_dog)
+
+# example of if statement
+# alphabet = "sljasdfakdsjlkajslfdkjaasdklfjsldjfiels"
+# if ("z" in alphabet):
+#     print("Yay - I found a Z char")
+    
+# example of a for loop
+# for i in range(1, 6):
+#     print(i)
+
+# example of lists (arrays)
+# foods = ["carrots", "kale", "beets"]
+# for food in foods:
+#     print("Hello, I currently have {} in the garden.".format(food))
+    
+    
+# example of function in python
+# def sum(num1, num2):
+#     return num1 + num2
+
+# total = sum(352, 38239)
+# print(total)
+    
+# example of built in list methods in py
+# colors = ["red", "yellow", "purple", "green"]
+# colors_length = len(colors)
+# print(colors_length)
+
+# colors.append("blue")
+# print(colors)
+
+# colors_length = len(colors)
+# print(colors_length)
+
+# example of mathematical list operations
+# batting_avgs = [.328, .348, .293, .293]
+# sum_avgs = sum(batting_avgs)
+# true_avg = sum_avgs / len(batting_avgs)
+# print(true_avg)
+
